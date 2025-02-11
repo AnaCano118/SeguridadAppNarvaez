c3f4e5e05b17ed42782dc8c02f46fb5b4417f13a
AnaCano <20223tn118@utez.edu.mx>
2025-02-10 18:06:07 -0600

    Errores

---
+++ Errores/src/test/java/com/canoAna/Errores/ErroresCanoNarvezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canoAna.Errores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErroresCanoNarvezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
